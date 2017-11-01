ea4aaa0de0948ae166f38828ff186b425b704711
twumm <martint.mensah@gmail.com>
2017-11-01 17:26:30 +0000

    Add function to make each cat unique with its own params

--- js/app.js
+++ js/app.js
@@ -1,27 +1,78 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('some link here');
-    this.catNicknames = ko.observableArray([
-        { nickname: 'Toyer' },
-        { nickname: 'Toyar' },
-        { nickname: 'Toyir' },
-    ]);
+var initialCats = [{
+        clickCount: 0,
+        name: 'Mindy',
+        imgSrc: 'img/cat1.jpg',
+        nicknames: [
+            { nickname: 'Miner' },
+            { nickname: 'Minar' },
+            { nickname: 'Minir' },
+        ]
+    },
+    {
+        clickCount: 0,
+        name: 'Kitty Galore',
+        imgSrc: 'img/cat2.jpg',
+        nicknames: [
+            { nickname: 'Toyer' },
+            { nickname: 'Toyar' },
+            { nickname: 'Toyir' },
+        ]
+    },
+    {
+        clickCount: 0,
+        name: 'Kitty Row',
+        imgSrc: 'img/cat3.jpg',
+        nicknames: [
+            { nickname: 'Kiter' },
+            { nickname: 'Kitar' },
+            { nickname: 'Kitir' },
+        ]
+    },
+    {
+        clickCount: 0,
+        name: 'Kitty Saf',
+        imgSrc: 'img/cat4.jpg',
+        nicknames: [
+            { nickname: 'Safer' },
+            { nickname: 'Safar' },
+            { nickname: 'Safir' },
+        ]
+    },
+    {
+        clickCount: 0,
+        name: 'Kitty Ju',
+        imgSrc: 'img/cat5.jpg',
+        nicknames: [
+            { nickname: 'Juyer' },
+            { nickname: 'Juyar' },
+            { nickname: 'Juyir' },
+        ]
+    }
+]
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    // this.imgAttribution = ko.observable('some link here');
+    this.nicknames = ko.observableArray(data.nicknames);
+    // this.catLevel = ko.observable(catLevel);
 
     // function to change cat level name using computed observables?
     this.catLevel = ko.computed(function() {
+        var levelName = ""
         if (this.clickCount() <= 10) {
-            return "Newborn";
+            levelName = "Newborn";
         } else if (this.clickCount() <= 20) {
-            return "Infant";
+            levelName = "Infant";
         } else if (this.clickCount() <= 50) {
-            return "Teen";
+            levelName = "Teen";
         } else if (this.clickCount() <= 100) {
-            return "Adult";
+            levelName = "Adult";
         } else {
-            return "Grown and Sexy";
+            levelName = "Grown and Sexy";
         }
+        return levelName;
 
         // one liner not working :/
         // this.catLevel = (this.clickCount() <= 2 ? "Newborn" : this.clickCount() <= 5 ?
@@ -31,13 +82,28 @@ var Cat = function() {
 }
 
 var ViewModel = function() {
+    // self will map to the ViewModel
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    // loop through initialCat list and push into catList array
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
     // new/current cat
-    this.currentCat = ko.observable(new Cat());
+    this.currentCat = ko.observable(this.catList()[0]);
 
 
-    this.incrementCounter = function() {
+    self.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
         // this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+
+    // set the cat when it is clicked
+    self.setCat = function(clickedCat) {
+        self.currentCat(clickedCat);
     };
 }
 
